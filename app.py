7a22ec82f0aebfae1ff50ffbe27fc9073c7376a1
tanis <tk009@lvc.edu>
2024-11-10 01:00:32 -0500

    initial API

---
+++ app.py
@@ -0,0 +1,18 @@
+from auth_token import auth_token
+from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline
+from io import BytesIO
+import base64
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
