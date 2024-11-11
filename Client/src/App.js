852399c8b5f5a880697e0be0f5ea48e748c67eda
tanis <tk009@lvc.edu>
2024-11-11 00:13:32 -0500

    React app successfully makes API call to generate an image

---
+++ Client/src/App.js
@@ -0,0 +1,40 @@
+import {
+    ChakraProvider,
+    Heading,
+    Container,
+    Text,
+    Input,
+    Button,
+    Wrap
+}
+from "@chakra-ui/react"
+
+import axios from "axios"
+import {useState} from "react"
+
+const App = () => {
+
+    const [image, updateImage] = useState()
+    const [prompt, updatePrompt] = useState()
+
+    const generate = async prompt =>{
+        const result = await axios.get(`http://127.0.0.1:8000/?prompt=${prompt}`)
+        updateImage(result.data)
+    }
+
+    return(
+        <ChakraProvider>
+            <Container>
+                <Heading>Stable Diffusion🎨</Heading>
+
+                <Wrap>
+                    <Input value={prompt} onChange={e => updatePrompt(e.target.value)} width={"350px"}></Input>
+                    <Button onClick={e => generate(prompt)} colorScheme={"yellow"}>Generate</Button>
+                </Wrap>
+            </Container>
+        </ChakraProvider>
+    );
+
+};
+
+export default App
